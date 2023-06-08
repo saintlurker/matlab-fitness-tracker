51841a855c4d8c385314fbe7b1b30f9be9f43fda
saintlurker <135952510+saintlurker@users.noreply.github.com>
2023-06-08 15:53:28 +0300

    Delete calculate_distance.m

--- calculate_distance.m
+++
@@ -1,7 +0,0 @@
-function dist = calculate_distance(long,lat,earth)
-    for i = 1:(length(lat)-1)
-        coord_dist(i) = [distance(lat(i),long(i),lat(i+1),long(i+1))]
-    end
-    dist = coord_dist.*earth./360
-end
-
